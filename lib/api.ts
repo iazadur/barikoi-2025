6caa68974fddbceb6c0b68b762ba9d261202cc78
iazadur <iamazadur@gmail.com>
2025-06-01 10:54:16 +0600

    feat: cache issue fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,23 @@
-
 const getPois = async () => {
-    const response = await fetch("https://mharmony.barikoimaps.dev/images")
+    const response = await fetch("https://mharmony.barikoimaps.dev/images", {
+        cache: 'no-store'
+    })
     const data = await response.json()
     console.log(data, "pois")
     return data
 }
 const approvePoi = async (poiId: string) => {
-    const response = await fetch(`https://mharmony.barikoimaps.dev/approve-image/${poiId}`)
+    const response = await fetch(`https://mharmony.barikoimaps.dev/approve-image/${poiId}`, {
+        cache: 'no-store'
+    })
     const data = await response.json()
     console.log(data, "approved")
     return data
 }
 const rejectPoi = async (poiId: string) => {
-    const response = await fetch(`https://mharmony.barikoimaps.dev/reject-image/${poiId}`)
+    const response = await fetch(`https://mharmony.barikoimaps.dev/reject-image/${poiId}`, {
+        cache: 'no-store'
+    })
     const data = await response.json()
     console.log(data, "rejected")
     return data
